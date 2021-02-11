4b872dbd9bc623ff96902bf47b4dc74424cf6983
Ana Rachel <euanarachel@outlook.com>
2021-02-11 16:13:23 -0300

    another nice test

---
+++ tanjiro_teste.js
@@ -0,0 +1,36 @@
+const kamadoTanjiro = {
+  name: "Kamado Tanjiro",
+  age: 15,
+  familyMembers: {
+    alive: {
+    sister: "Kamado Nezuko",
+    },
+    dead: {
+      father: "Kamado Tanjuro",
+      mother: "Kamado Kie",
+      brother_1: "Kamado Shigeru",
+      brother_2: "Kamado Rokuta",
+      brother_3: "Kamado Takeo",
+      sister_2: "Kamado Hanako",
+    }
+   },
+  abilities: {
+    sense: "sense_of_the_smell",
+    breath_of_water: {
+      firstForm: "Minamogiri",
+      secondForm: "Mizuguruma",
+      thirdForm: "Ryuuryuu Mai",
+      fourthForm: "Uchishio",
+      fifthForm: "Kanten No Jyu",
+      sixthForm: "Nejire Uzu",
+      seventhForm: "Shizuku Wa Mondzuki",
+      eigthForm: "Taktsubo",
+      ninthForm: "Suiryuu Shibuki",
+      tenthForm: "Seisei Ruten",
+      // evelenthForm: "Nagi",
+    },
+    breath_of_fire: {
+      unknowForm: "Dance of the Fire God",
+    },
+  },
+ }
