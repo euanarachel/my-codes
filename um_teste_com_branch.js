ae5c6eb3a23137ed91bd9df0ba7e6f9dd6dedc47
Ana Rachel <euanarachel@outlook.com>
2021-02-11 21:39:20 -0300

    um teste com branch

---
+++ um_teste_com_branch.js
@@ -0,0 +1,89 @@
+const kamadoTanjiro = {
+    name: "Kamado Tanjiro",
+    age: 15,
+    familyMembers: {
+      alive: {
+      sister: "Kamado Nezuko",
+        brother_in_law: "Agatsuma Zenitsu"
+      },
+      dead: {
+        father: "Kamado Tanjuro",
+        mother: "Kamado Kie",
+        brother_1: "Kamado Shigeru",
+        brother_2: "Kamado Rokuta",
+        brother_3: "Kamado Takeo",
+        sister_2: "Kamado Hanako",
+      }
+     },
+    abilities: {
+      sense: "sense_of_the_smell",
+      breath_of_water: {
+        firstForm: "Minamogiri",
+        secondForm: "Mizuguruma",
+        thirdForm: "Ryuuryuu Mai",
+        fourthForm: "Uchishio",
+        fifthForm: "Kanten No Jyu",
+        sixthForm: "Nejire Uzu",
+        seventhForm: "Shizuku Wa Mondzuki",
+        eigthForm: "Taktsubo",
+        ninthForm: "Suiryuu Shibuki",
+        tenthForm: "Seisei Ruten",
+        // evelenthForm: "Nagi",
+      },
+      breath_of_fire: {
+        unknowForm: "Dance of the Fire God",
+      },
+    },
+   }
+  
+  const inosukeHashibira = {
+    name: "Inosuke Hashibira",
+    age: 15,
+    familyMembers: {
+      alive: {
+        wife: "Aoi Kanzaki"
+      },
+      dead: {
+        mother: "Kotoha Hashibira"
+      }
+    },
+    abilities: "sense_of_the_touch",
+    breath_of_beast: {
+      firstFang: "Pierce",
+      secondFang: "Rip and Tear",
+      thirdFang: "Devour",
+      fourthFang: "Slice 'n' Dice",
+      fifthFang: "Crazy Cutting",
+      sixthFang: "Palisade Bite",
+      seventhFang: "Spatial Awareness",
+      eigthFang: "Explosive Rush",
+      ninthFang: "Extending Bendy Slash",
+      tenthFang: "Whirling Fangs",
+      // bonusFang: "Sudden Throwing Strike"
+    }
+  }
+  
+  const zenitzuAgatsuma ={
+    name: "Zenitsu Agatsuma",
+    age: 15,
+    familymembers: {
+      alive: {
+        wife: "Kamado Nezuko",
+        brother_in_law: "Kamado Tanjiro"
+      },
+      dead: {
+        grandfather: "Jigoro Kuwajima",
+        cousin: "Kaigaku"
+      }
+    },
+    abilities: "sense_of_the_hearing",
+    breath_of_thunder: {
+      firstForm: {
+        principal: "Thunderclap and Flash",
+        extension1: "Six Fold",
+        extension2: "Eight Fold",
+        extension3: "God Speed",
+      },
+      seventhForm: "Honoikazuchi no Kami"
+    }
+  }
